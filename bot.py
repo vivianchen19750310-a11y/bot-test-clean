3a9c4ae8430cfab2dd356b88afd09a1262935c1e
vivianchen19750310-a11y <vivianchen19750310@gmail.com>
2026-04-25 01:54:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
+def run():
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
 
-creds = Credentials.from_service_account_file("key.json", scopes=scope)
-client = gspread.authorize(creds)
+    creds = Credentials.from_service_account_file("key.json", scopes=scope)
+    client = gspread.authorize(creds)
 
-sheet = client.open("bot-test").sheet1
+    sheet = client.open("bot-test").sheet1
 
-data = sheet.get_all_records()
+    data = sheet.get_all_records()
 
-print("讀到資料：")
-print(data)
+    print("讀到資料：")
+    print(data)
