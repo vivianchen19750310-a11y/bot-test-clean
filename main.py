97a874813df717e67d9e6a62f727d911192cdbb5
vivianchen19750310-a11y <vivianchen19750310@gmail.com>
2026-04-25 01:54:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import gspread
-from google.oauth2.service_account import Credentials
+from bot import run
 
-# 連線 Google API
-creds = Credentials.from_service_account_file("key.json")
-client = gspread.authorize(creds)
-
-# 開啟 Sheet（名字要完全一樣）
-sheet = client.open("你的Google Sheet名稱").sheet1
-
-# 讀取資料
-data = sheet.get_all_records()
-
-print("Sheet data:")
-print(data)
+run()
